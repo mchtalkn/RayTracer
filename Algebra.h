002bd9e61383da5cf2ca98929946f0ba1ba4913a
Selin Yildirim <selin@otsimo.com>
2021-11-13 19:32:10 +0300

    products

--- Algebra.h
+++ Algebra.h
@@ -1,8 +1,18 @@
 //
 // Created by Selin Yıldırım on 13.11.2021.
 //
+#include "parser.h"
 
 #ifndef GIT_ALGEBRA_H
 #define GIT_ALGEBRA_H
+
+class Algebra{
+    public:
+    float dotProduct(parser::Vec3f , parser::Vec3f);
+    float crossProduct(parser::Vec3f , parser::Vec3f);
+
+
+};
+
 
 #endif //GIT_ALGEBRA_H
