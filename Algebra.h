904514dacc798437f45ad45c886cdc795c11a1fe
mchtalkn <mucahit.a.yorganci@gmail.com>
2021-11-13 19:31:46 +0300

    Algebra +-

--- Algebra.h
+++ Algebra.h
@@ -1,8 +1,14 @@
 //
 // Created by Selin Yıldırım on 13.11.2021.
 //
-
+#include <iostream>
+#include "parser.h"
+#include "ppm.h"
+using namespace parser;
 #ifndef GIT_ALGEBRA_H
 #define GIT_ALGEBRA_H
+
+Vec3f operator +(Vec3f lhs, Vec3f rhs);
+Vec3f operator -(Vec3f lhs, Vec3f rhs);
 
 #endif //GIT_ALGEBRA_H
