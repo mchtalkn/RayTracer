4d497c75df1e22167627917e4cab32302ecee9f6
mchtalkn <mucahit.a.yorganci@gmail.com>
2021-11-13 18:48:19 +0300

    Add project files.

---
+++ ceng477hw1.cpp
@@ -0,0 +1,12 @@
+﻿// ceng477hw1.cpp : Defines the entry point for the application.
+//
+
+#include "ceng477hw1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
