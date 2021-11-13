680acb05be72e8f82ea1e31c99b6652180529afc
Selin Yildirim <selin@otsimo.com>
2021-11-13 21:15:30 +0300

    calculations are complete

--- Algebra.cpp
+++ Algebra.cpp
@@ -15,36 +15,58 @@ Vec3f operator+(Vec3f lhs, Vec3f& rhs)
 
 Vec3f operator-(Vec3f lhs, Vec3f& rhs)
 {
-	return Vec3f();
+    Vec3f r;
+    r.x = lhs.x - rhs.x;
+    r.y = lhs.y - rhs.y;
+    r.z = lhs.z -s rhs.z;
+    return r;
 }
 
 Vec3f operator*(Vec3f lhs, float rhs)
 {
-	return Vec3f();
+    Vec3f r;
+    r.x = lhs.x * rhs;
+    r.y = lhs.y * rhs;
+    r.z = lhs.z * rhs;
+    return r;
 }
 
 Vec3f operator*(float lhs, Vec3f& rhs)
 {
-	return Vec3f();
+    Vec3f r;
+    r.x = lhs * rhs.x;
+    r.y = lhs * rhs.y;
+    r.z = lhs * rhs.z;
+    return r;
 }
 
 float dotProduct(Vec3f& lhs, Vec3f& rhs)
 {
-	return 0.0f;
+    float r = lhs.x*rhs.x + lhs.y*rhs.y + lhs.z*rhs.z ;
+	return r;
 }
 
 Vec3f crossProduct(Vec3f& lhs, Vec3f& rhs)
 {
-	return Vec3f();
+    Vec3f r;
+    r.x= lhs.y*rhs.z - lhs.z*rhs.y ;
+    r.y= lhs.z*rhs.x - lhs.x*rhs.z ;
+    r.z= lhs.x*rhs.y - lhs.y*rhs.x ;
+    return r;
 }
 
 Vec3f hadamardProduct(Vec3f& lhs, Vec3f& rhs)
 {
-	return Vec3f();
+    Vec3f r;
+    r.x= lhs.x*rhs.x ;
+    r.y= lhs.y*rhs.y ;
+    r.z= lhs.z*rhs.z ;
+    return r;
 }
 
 float determinant(Vec3f v1, Vec3f v2, Vec3f v3)
 {
-	return 0.0f;
+    float r = v1.x*v2.y*v3.z  -  v1.x*v2.z*v3.y  -  v1.y*v2.x*v3.z  +  v1.y*v2.z*v3.x  +  v1.z*v2.x*v3.y  -  v1.z*v2.y*v3.z ;
+    return r;
 }
 
