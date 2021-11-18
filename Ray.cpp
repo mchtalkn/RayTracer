e24e4f97aebf6ac7649b0c41fcbee88358c6b9a0
mchtalkn <mucahit.a.yorganci@gmail.com>
2021-11-18 20:46:50 +0300

    render function for camera handler

--- Ray.cpp
+++ Ray.cpp
@@ -56,6 +56,31 @@ Vec3f Ray::calculateColor(Vec3f& intersection, Vec3f& normal, Material& material
 
 Vec3f Ray::calculateColor()
 {
+	/*
+	for (Sphere& s : parser::scene.spheres) {
+		tmpt = r.intersect(s);
+		if (tmpt > camera.near_distance && tmpt < t) {
+			t = tmpt;
+			sphere = &s;
+			bestType = 1;
+		}
+	}
+	for (Triangle& tr : parser::scene.triangles) {
+		tmpt = r.intersect(tr);
+		if (tmpt > camera.near_distance && tmpt < t) {
+			t = tmpt;
+			triange = &tr;
+			bestType = 2;
+		}
+	}
+	for (Sphere& s : parser::scene.spheres) {
+		tmpt = r.intersect(s);
+		if (tmpt > camera.near_distance && tmpt < t) {
+			t = tmpt;
+			sphere = &s;
+			bestType = 3;
+		}
+	}*/
     return Vec3f();
 }
 
