eae2c4485102b794e1f1d26f2a7d55932dbfbf38
mchtalkn <mucahit.a.yorganci@gmail.com>
2021-11-19 22:27:19 +0300

    process info bugfix

--- CameraHandler.cpp
+++ CameraHandler.cpp
@@ -50,7 +50,7 @@ void CameraHandler::render()
 	Ray r;
 	for (int j = 0; j < ny; j++) {
 		for (int i = 0; i < nx; i++) {
-			if ( (i == 0) && (((j*100) % ny/100*100) == 0)) {
+			if ( (i == 0) && (((j*100) % nx/100*100) == 0)) {
 				auto t_end = std::chrono::high_resolution_clock::now();
 				double elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end - t_start).count();
 				cout << "rendering... %" << ((j * 100) / nx) << " completed elapsed time: "<<(int) elapsed_time_ms/60/1000 << " minutes "<< ((int)elapsed_time_ms/1000%60)<<" seconds " << endl;
