cc156da13a9266f57dbc108c5a1632cad1845212
mchtalkn <mucahit.a.yorganci@gmail.com>
2021-11-19 13:17:08 +0300

    debug code deleted

--- SceneHandler.cpp
+++ SceneHandler.cpp
@@ -27,6 +27,5 @@ SceneHandler::SceneHandler(string input)
 
 void SceneHandler::render()
 {
-    for (int i = 0; i < cameras.size(); i++) cameras[i].render();
 	for (CameraHandler& c : cameras) c.render();
 }
